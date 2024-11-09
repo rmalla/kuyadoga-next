bae1be44638952ec75837116f5a52ec90dbc206d
rmalla <rmp6262@hotmail.com>
2024-11-09 13:57:08 +0000

    starting the add to cart functionality

---
+++ webstore-next-ssr-app/src/context/CartContext.js
@@ -0,0 +1,51 @@
+// src/context/CartContext.js
+
+import { createContext, useContext, useState, useEffect } from 'react';
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+    const [cart, setCart] = useState([]);
+
+    // Load cart data from local storage on initial load
+    useEffect(() => {
+        const storedCart = localStorage.getItem('cart');
+        if (storedCart) {
+            setCart(JSON.parse(storedCart));
+        }
+    }, []);
+
+    // Save cart data to local storage whenever the cart changes
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart]);
+
+    const addToCart = (product) => {
+        setCart((prevCart) => {
+            const itemIndex = prevCart.findIndex((item) => item.productId === product.id);
+            if (itemIndex > -1) {
+                const updatedCart = [...prevCart];
+                updatedCart[itemIndex].quantity += 1;
+                return updatedCart;
+            } else {
+                return [...prevCart, { ...product, quantity: 1 }];
+            }
+        });
+    };
+
+    const removeFromCart = (productId) => {
+        setCart((prevCart) => prevCart.filter((item) => item.productId !== productId));
+    };
+
+    const clearCart = () => {
+        setCart([]);
+    };
+
+    return (
+        <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+export const useCart = () => useContext(CartContext);
